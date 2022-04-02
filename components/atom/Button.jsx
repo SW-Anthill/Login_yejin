0dc440f52c70235cddf122fe026ad3f24ceb1e6a
yejin <yeajin357@naver.com>
2022-04-02 21:14:02 +0900

    Add atom/Button component

---
+++ components/atom/Button.jsx
@@ -0,0 +1,8 @@
+import react from "react"
+import tw from "twin.macro"
+
+const CustomButton = tw.button`outline-none rounded-[40px] text-[24px] text-center py-[12px] w-full border-skyblue-300 border-[1px] text-skyblue-300 hocus:bg-skyblue-300 hocus:text-white`
+
+export default function Button({ label, onClick }) {
+  return <CustomButton onClick={onClick}>{label}</CustomButton>
+}
