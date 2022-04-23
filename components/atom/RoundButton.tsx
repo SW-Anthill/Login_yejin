0c4fc90234220db2ffdf7036f4e8619b52905366
yejin <yeajin357@naver.com>
2022-04-23 19:38:09 +0900

    use formik

--- components/atom/RoundButton.tsx
+++ components/atom/RoundButton.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import tw from "twin.macro"
 
-const CustomButton = tw.button`outline-none rounded-[40px] text-[24px] text-center py-[12px] w-full border-skyblue-300 border-[1px] text-skyblue-300 hocus:bg-skyblue-300 hocus:text-white`
+const CustomButton = tw.button`outline-none rounded-[40px] text-[24px] text-center py-[12px] w-full border-skyblue-300 border-[1px] text-skyblue-300 hover:bg-skyblue-300 hover:text-white`
 
 type Props = {
   label?: string
--- pages/create.tsx
+++ pages/create.tsx
@@ -1,28 +1,64 @@
 import React, { useState } from "react"
 import tw from "twin.macro"
 import BoxInput from "../components/atom/BoxInput"
-import Button from "../components/atom/RoundButton"
+import RoundButton from "../components/atom/RoundButton"
 import CategoryDivider from "../components/atom/CategoryDivider"
 import SquareButton from "../components/atom/SquareButton"
 import { InputLabel } from "../components/styles"
 import Textarea from "../components/atom/Textarea"
-import RoundButton from "../components/atom/RoundButton"
+import { useFormik } from "formik"
 
-const Container = tw.div`w-[600px] mx-auto flex flex-col items-center justify-center`
+const Container = tw.form`w-[600px] mx-auto flex flex-col items-center justify-center`
 
 const Title = tw.div`font-bold w-full text-[40px] mt-[70px]`
 
 const LabelButton = tw.label`whitespace-nowrap cursor-pointer flex items-center rounded-[2px] text-[16px] bg-green-300 text-center h-[46px] px-[12px] text-white`
 
+type InputValues = {
+  orderName?: string
+  desc?: string
+  storeName?: string
+  storeAddress?: string
+  minOrderPrice?: number
+  menuName?: string
+  menuCount?: number
+  menuPrice?: number
+}
+
 export default function Create() {
   const [thumbnail, setThumbnail] = useState<File>()
+  const { handleChange, handleSubmit, values } = useFormik<InputValues>({
+    initialValues: {},
+    onSubmit: (values) => {
+      if (
+        !Object.values(values).every(
+          (value) => value !== undefined && value !== ""
+        ) ||
+        !thumbnail
+      ) {
+        alert("내용을 모두 입력해주세요.")
+        return
+      }
+      localStorage.setItem("orderName", values.orderName ?? "")
+      localStorage.setItem("desc", values.desc ?? "")
+      localStorage.setItem("thumbnail", URL.createObjectURL(thumbnail))
+      localStorage.setItem("orderName", values.orderName ?? "")
+      const item = {
+        name: values.menuName,
+        quantity: values.menuCount,
+        price: values.menuPrice,
+      }
+      localStorage.setItem("items", JSON.stringify(item))
+    },
+  })
+
   return (
-    <Container>
+    <Container onChange={handleChange} onSubmit={handleSubmit}>
       <Title>배딜 생성</Title>
       <CategoryDivider category="배딜 설명" style={{ marginTop: "27px" }} />
       <div tw="w-full grid gap-[8px]">
-        <BoxInput label="배딜 제목" />
-        <Textarea label="상세 설명" />
+        <BoxInput label="배딜 제목" name="orderName" value={values.orderName} />
+        <Textarea label="상세 설명" name="desc" value={values.desc} />
         <div tw="flex items-center">
           <input
             type="file"
@@ -41,13 +77,22 @@ export default function Create() {
       </div>
       <CategoryDivider category="가게 정보" style={{ marginTop: "60px" }} />
       <div tw="w-full grid gap-[8px]">
-        <BoxInput label="이름" />
+        <BoxInput label="이름" name="storeName" />
         <div tw="flex items-center">
           <BoxInput label="위치" tw="w-[356.5px]" />
           <SquareButton label="우편 번호 찾기" />
         </div>
-        <BoxInput label=" " placeholder="상세 주소 입력" />
-        <BoxInput label="주문 최소 금액" />
+        <BoxInput
+          label=" "
+          placeholder="상세 주소 입력"
+          name="storeAddress"
+          value={values.storeAddress}
+        />
+        <BoxInput
+          label="주문 최소 금액"
+          name="minOrderPrice"
+          value={values.minOrderPrice}
+        />
       </div>
       <CategoryDivider category="메뉴 정보" style={{ marginTop: "60px" }} />
       <div tw="grid grid-flow-col gap-[5px]">
@@ -57,20 +102,21 @@ export default function Create() {
         </div>
         <div>
           <InputLabel>메뉴 이름</InputLabel>
-          <BoxInput tw="w-[237px]" />
+          <BoxInput tw="w-[237px]" name="menuName" value={values.menuName} />
         </div>
         <div>
           <InputLabel>수량</InputLabel>
-          <BoxInput tw="w-[103px]" />
+          <BoxInput tw="w-[103px]" name="menuCount" value={values.menuCount} />
         </div>
         <div>
           <InputLabel>가격</InputLabel>
-          <BoxInput tw="w-[134px]" />
+          <BoxInput tw="w-[134px]" name="menuPrice" value={values.menuPrice} />
         </div>
       </div>
       <RoundButton
         label="완료"
-        tw="text-[16px] text-green-300 border-green-300 w-[114px] mt-[40px] ml-auto hocus:bg-green-300"
+        type="submit"
+        tw="text-[16px] text-green-300 border-green-300 w-[114px] mt-[40px] ml-auto hover:bg-green-300"
       />
     </Container>
   )
