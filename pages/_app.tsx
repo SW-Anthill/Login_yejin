66b0fd1b847ceddcfb031c544f8c8b8b5c36533c
yejin <yeajin357@naver.com>
2022-04-30 21:00:16 +0900

    Install react-helmet and react-daum-postcode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import React from "react"
 import { AppProps } from "next/app"
+import Script from "next/script"
 import { GlobalStyles } from "twin.macro"
+import { Helmet } from "react-helmet"
 import "../styles/tailwind.globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Anthill</title>
+        <script src="https://ssl.daumcdn.net/dmaps/map_js_init/postcode.v2.js?autoload=false" />
+      </Helmet>
       <GlobalStyles />
       <Component {...pageProps} />
     </>
